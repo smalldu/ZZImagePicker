b75a6ddbf0631ad87199f8f15efd0925c339d523
smalldu <363958265@qq.com>
2016-05-20 11:06:15 +0800

    请求访问权限的封装

--- ZZImagePicker/ViewController.swift
+++ ZZImagePicker/ViewController.swift
@@ -22,48 +22,14 @@ class ViewController: UIViewController {
     
     @IBAction func goPhoto(sender: AnyObject) {
         
-        
-        authorize()
-        
+        self.zz_presentPhotoVC(6) { (assets) in
+            
+        }
         
     }
 
     
-    func authorize(status: PHAuthorizationStatus = PHPhotoLibrary.authorizationStatus()){
-        switch status {
-        case .Authorized:
-            self.zz_presentPhotoVC(6){ (assets) in
-                print(assets.count)
-            }
-        case .NotDetermined:
-            // 请求授权
-            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    self.authorize(status)
-                })
-            })
-        default: ()
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            let alertController = UIAlertController(title: "访问相册受限",
-                message: "点击“设置”，允许访问您的相册",
-                preferredStyle: .Alert)
-            
-            let cancelAction = UIAlertAction(title:  "取消", style: .Cancel, handler:nil)
-            
-            let settingsAction = UIAlertAction(title: "设置", style: .Default, handler: { (action) -> Void in
-                let url = NSURL(string: UIApplicationOpenSettingsURLString)
-                if let url = url where UIApplication.sharedApplication().canOpenURL(url) {
-                    UIApplication.sharedApplication().openURL(url)
-                }
-            })
-            
-            alertController.addAction(cancelAction)
-            alertController.addAction(settingsAction)
-            
-            self.presentViewController(alertController, animated: true, completion: nil)
-        })
-        }
-    }
+   
 }
 
 
--- ZZImagePicker/ZZPhotoPicker/UICollectionView+Add.swift
+++ ZZImagePicker/ZZPhotoPicker/UICollectionView+Add.swift
@@ -45,7 +45,43 @@ extension NSIndexSet{
 
 extension UIViewController{
 
+    private func authorize(status: PHAuthorizationStatus = PHPhotoLibrary.authorizationStatus())->Bool{
+        switch status {
+        case .Authorized:
+            return true
+        case .NotDetermined:
+            // 请求授权
+            PHPhotoLibrary.requestAuthorization({ (status) -> Void in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.authorize(status)
+                })
+            })
+        default: ()
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let alertController = UIAlertController(title: "访问相册受限",
+                message: "点击“设置”，允许访问您的相册",
+                preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title:  "取消", style: .Cancel, handler:nil)
+            
+            let settingsAction = UIAlertAction(title: "设置", style: .Default, handler: { (action) -> Void in
+                let url = NSURL(string: UIApplicationOpenSettingsURLString)
+                if let url = url where UIApplication.sharedApplication().canOpenURL(url) {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            })
+            
+            alertController.addAction(cancelAction)
+            alertController.addAction(settingsAction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        })
+        }
+        return false
+    }
+    
     func zz_presentPhotoVC(maxSelected:Int,completeHandler:((assets:[PHAsset])->())?) -> ZZPhotoViewController?{
+        guard authorize() else { return nil }
         if let vc = UIStoryboard(name: "ZZImage", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("photoVC") as? ZZPhotoViewController{
             vc.completeHandler = completeHandler
             vc.maxSelected = maxSelected
